be82b3726da07178a6fa934f0a96fa6b20722b42
Kevin <173805497+KevinLabrosse@users.noreply.github.com>
2025-03-14 12:54:45 +0100

    Updated semantic notes

--- semantic_examples.py
+++ semantic_examples.py
@@ -5,9 +5,11 @@ x,y,z = input()     # x : "3", y : "6", z: "2"
 
 ### input_and_convert ###
 t = 3
-x = input(...t)     # x : ["4","8","1"]
+x = input(...t, string)     # x : ["4","8","hello"], converts input to string
 
-x = input(...t, int)     # x : [4,2,1]
+x = input(...t, int)     # x : [4,2,1], define type
+
+x = input("#", int)  # Read input as int until the string is read
 
 ### output ###
 print("Hello: %d", val)
@@ -24,8 +26,8 @@ for(i, i < 7, i++) {
 
 
 ### Match Data ###
-entytit PERSON = { age : int, name : string, id : string, allow : bool }
-entytit BARS = { ... }
+Entity PERSON = { age : int, name : string, id : string, allow : bool }
+Entity BARS = { ... }
 
 
 PERSON person1 = (22, "Torben", "xdxa432", true)
@@ -38,7 +40,7 @@ match(list) {
     case ( PERSON(age, id) : age < 18 && id ) {
         wait to 18
     }
-    case ( PERSON(id) : id == undef) {
+    case ( BARS(id) : id == undef) {
         delete
     }
 }
@@ -57,15 +59,28 @@ match(person in list) {
 }
 
 
-### Dicstra ###
+### Dijkstra ###
 ## vi har deffineret edges_list, matrix, adj_list i sproget.
-edges_list input = input()
-matrix input = input()
-adj_list input = input()
+Edge_list input = input()  # graph
+Matrix input = input()
+Adj_list input = input()
 
-best_paths = dicstra(input, source_note, input_type)  
-best_paths = dicstra(input, source_note)              #dicstra ved her hvilken type af graf den arbejder med
+best_paths = dijkstra(input, source_note, input_type)  
+best_paths = dijkstra(input, source_note)              #dicstra ved her hvilken type af graf den arbejder med
 
+graph = input(...m, [int, int, int])  # Read m edges with weights
+dist = graph.dijkstra(source)
+
+### Data Types ###
+grid = Grid(n, m, 0)
+grid[i][j] = value  # Replace value in grid index i,j
+grid.neighbours(i, j)   # returns: [top, right, bottom, left]
+
+matrix = input(...n, Matrix, int)    # Reads integer input as type Matrix
+matrix = input(...n, Matrix, int, INF?)  # Define value used in input to show null on an index
+matrix.toEdgeList()
+matrix.toAdjList()
+matrix.toMatrix()
 
 ### complexity måler ### ###outputter til bruger om de når deres mål
 @complexity, time O(log n), space (n).
@@ -81,3 +96,6 @@ function add_two_lists(A, B)
 
 @parallel, custom(a+b)
 function add_two_lists(A, B)
+
+### QuickSort ###
+list.QuickSort(preimplemented_comparators = ASC?, If_Struct_what_property?, CUSTOM_COMPARATOR?) # ? = optional argument
