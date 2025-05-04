6647968c48ef27f5c31637211156956c36ebe7f8
Kevin <173805497+KevinLabrosse@users.noreply.github.com>
2025-05-05 00:14:27 +0200

    Added llvm fix to bash code
    The function "memcpy" that we use to copy content of an array in the memory to another memory space requires spacing to avoid error while maintaining the llvm optimization options. To do this, we have to manually do it through the terminal, as the OCaml LLVM binding does not accommodate the newer version of llvm.memcpy

--- run.sh
+++ run.sh
@@ -13,6 +13,12 @@ dune build
 echo "Running compiler on $TEST_FILE..."
 dune exec compiler "$TEST_FILE"
 
+# The memcpy function in LLVM IR may not have the correct alignment.
+# This is due to the OCaml LLVM binding does not facilitate the "align" attribute, which is needed for the memcpy function to work correctly in some cases.
+# We need to fix the alignment in the generated LLVM IR manually.
+echo "Fixing alignment for the memcpy function..."
+sed -i '' -E 's/call void @llvm\.memcpy\.p0\.p0\.i64\(ptr ([^,]+), ptr ([^,]+),/call void @llvm.memcpy.p0.p0.i64(ptr align 8 \1, ptr align 8 \2,/' output.ll
+
 echo "Compiling generated LLVM IR..."
 clang output.ll -o program
 
